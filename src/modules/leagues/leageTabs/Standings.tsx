48d9310ae596adfbf17464f9c04deeb827a65953
Ts-geek73 <kushal.modi@techstaunch.com>
2026-01-07 17:26:57 +0530

    style: Reformat green button variant and reorder import statement.

--- src/modules/leagues/leageTabs/Standings.tsx
+++ src/modules/leagues/leageTabs/Standings.tsx
@@ -3,10 +3,10 @@ import { useRouter } from "next/navigation";
 import React from "react";
 
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 
-import { Button } from "@/components/ui/button";
 import { useLeagueStandings } from "../hooks";
 import LeagueBanner from "../LeagueBanner";
 
