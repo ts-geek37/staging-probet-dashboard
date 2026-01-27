80c59dea8bd5d8d7d380d5a44c30f56f53fdcbef
Ts-geek73 <kushal.modi@techstaunch.com>
2026-01-27 11:20:28 +0530

    Refactor: Redesign the accurate prediction card UI and enhance home page data destructuring for improved clarity and robustness.

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -24,19 +24,29 @@ interface Props {
 }
 
 const Home: React.FC<Props> = ({ initialHome }) => {
-  const { data, isLoading } = useHome(initialHome);
-
-  if (!data) return null;
+  const {
+    sections: {
+      live_now,
+      starting_soon,
+      recently_finished,
+      accuratePredictions,
+    },
+    topLeagues,
+    popularTeams,
+    news,
+    isLoading,
+  } = useHome(initialHome);
 
   return (
     <>
       <Banner />
       <div className="max-w-7xl w-full mx-auto px-4">
         <VIPBanner />
-        {data.sections.live_now.length > 0 && (
+
+        {live_now.length > 0 && (
           <>
             <LiveMatchCards
-              initialMatches={data.sections.live_now}
+              initialMatches={live_now}
               href={`/matches?status=${MatchListStatus.LIVE}`}
               scopeInfo={{ scope: LiveScopeEnum.GENERAL }}
               className="py-10 md:py-20"
@@ -45,30 +55,32 @@ const Home: React.FC<Props> = ({ initialHome }) => {
             <VIPBanner />
           </>
         )}
-        {data.sections.starting_soon.length > 0 && (
+
+        {starting_soon.length > 0 && (
           <>
-            <UpcomingMatchCards matches={data.sections.starting_soon} />
+            <UpcomingMatchCards matches={starting_soon} />
             <VIPBanner />
           </>
         )}
-        {data.sections.recently_finished.length > 0 && (
+
+        {recently_finished.length > 0 && (
           <>
-            <FinishedMatchesCards matches={data.sections.recently_finished} />
+            <FinishedMatchesCards matches={recently_finished} />
             <VIPBanner />
           </>
         )}
-        <TopLeagues
-          topLeagues={data.top_leagues}
-          standings={data.popular_teams}
-        />
+
+        <TopLeagues topLeagues={topLeagues} standings={popularTeams} />
+
         <VIPBanner />
         <PredictionBanner />
         <VIPBanner />
         <AccuratePredictions
-          predictions={data.sections.accuratePredictions}
+          predictions={accuratePredictions}
           isLoading={isLoading}
         />
-        <LatestNews news={data.news ?? []} />
+
+        <LatestNews news={news} />
         <Testimonials />
       </div>
     </>
